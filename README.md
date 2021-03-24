e4c4ee4811572a941488b75ef1419bf315642053
claire181129 <jtq0521@163.com>
2021-03-24 18:47:20 +0800

    2021-03-24 update: daily update

--- README.md
+++ README.md
@@ -10,11 +10,11 @@ Solutions of algorithm questions from leetcode and other website.
 
 about algorithm question:
 1. case study, manually compute the example case, see how you get the result.
-2. case coverage, what situation is covered in your code? What is not?
+2. case coverage, what situation is covered in your code? What is not? From first line of code, record case in comment, write debug output for this case
 3. write a code(whatever it is, a mess or pass code), and test it, get the result
 4. if not pass, code trace, till pass
 5. Try to come up with a variety of problem-solving methods
-6. try to come up with a elegant algorithm
-7. there's no target, you're just talking, say what you want, say what appear in your head.
-8. limit , quit
-9. theory vs practice, reality is ugly, but it's alive and playable, and can live better.
+6. Try to come up with an elegant algorithm
+7. There's no target, you're just talking, say what you want, say what appear in your head.
+8. Limit , quit
+9. Theory vs practice, reality is ugly, but it's alive and playable, and can live better.
--- nowcoder-huawei/main.cxx
+++ nowcoder-huawei/main.cxx
@@ -38,33 +38,305 @@ int computeExpression(std::string expression, bool& legal)
     }
     else
     {
+        // compute * or /
         size_t index = expression.find_first_of("*/");
         auto size = expression.size();
         if(index == std::string::npos)
         {
             std::cout<<"\t"<<" no * or / to compute"<<std::endl;
-            //compute + or -
+            //compute + or -, no * or / operator
             index = expression.find_first_of("+-");
             if(index == std::string::npos)
             {
                 //no any operator, it's a number
+                std::cout<<"\t no + or - operator found, expression is pure number, expression:"<<expression<<std::endl;
                 result = std::stoi(expression);
             }
             else
             {
+                std::cout<<"\t found + or - operator, index:"<<index<<std::endl;
                 //1. it's a symbol
                 //2. it's a operator
                 int leftIndex = index-1;
+                //1. leftIndex<0, index == 0, it must be a symbol
+                //2. leftIndex>=0, index>=0, must be a operator
                 if(leftIndex<0)
                 {
-                    //1. it's a legal symbol, or a illegal character, there's no expression to compute
-                    //2. there's still expression to compute
-                    std::cout<<"\t"<<" + or - is the first character, no left operand"<<std::endl;
+                    //1. leftIndex<0, index == 0, it must be a symbol, no leftoperand
+                    std::cout<<"\t leftIndex < 0, + or - is the first character, no left operand, leftIndex:"<<leftIndex<<std::endl;
                     int rightIndex = index+1;
-                    while(()&&(expression[rightIndex]))
-                    
+                    while((rightIndex<size)&&(expression[rightIndex] >= 48)&&(expression[rightIndex]<=57))
+                    {
+                        rightIndex++;
+                    }
+                    //rightIndex will point to:
+                    //1. rightIndex >= size, out of range
+                    //2. rightIndex < size, meet not number character
+                    if(rightIndex<size)
+                    {
+                        std::cout<<"\t meet non number character, rightIndex:"<<rightIndex<<std::endl;
+                        //2. rightIndex < size, meet not number character
+                        //the position is like
+                        //1. index,rightIndex
+                        //2. index,...,rightIndex
+                        if(rightIndex == index+1)
+                        {
+                            //1. index,rightIndex, rightIndex point to non number character, in range
+                            //index is a symbol
+                            std::cout<<"\t rightIndex == index+1"<<std::endl;
+                            while((rightIndex<size)&&((expression[rightIndex] == '+')||(expression[rightIndex] == '-')))
+                            {
+                                rightIndex++;
+                            }
+                            //1. rightIndex is out of range
+                            //2. rightIndex point to a number character
+                            if(rightIndex >=size)
+                            {
+                                //1. rightIndex is out of range
+                                std::cout<<"\t illegal, no right operand, all character after + or - is + or -, rightIndex out of range, rightIndex:"<<rightIndex<<std::endl;
+                                legal = false;
+                                result = 0;
+                            }
+                            else
+                            {
+                                //2. rightIndex point to a number character
+                                //remove extra symbol
+                                std::cout<<"\t from index:"<<index<<" to rightIndex-1:"<<rightIndex-1<<" is all symbol:"<<expression<<std::endl;
+                                int negative = 1;
+                                for(int i=index+1;i<rightIndex;i++)
+                                {
+                                    if(expression[i] == '-')
+                                        negative+=-1;
+                                }
+                                std::string substr;
+                                if(negative == 1) 
+                                    substr = "";
+                                else
+                                    substr = "-";
+                                expression.insert(index,substr);
+                                expression.erase(index+substr.size(),rightIndex-index);
+                                std::cout<<"\tafter remove symbol, expression:"<<expression<<" substr:"<<substr<<std::endl;
+                                result = computeExpression(expression,legal);
+                            }
+                        }
+                        else
+                        {
+                            //leftIndex<0, index == 0, it must be a symbol, no leftoperand
+                            //rightIndex < size, meet not number character
+                            //2. index, ..., rightIndex, there is number between index and rightIndex
+                            leftIndex = index;
+                            index = rightIndex;
+                            std::cout<<"\t has left operand, leftIndex:"<<leftIndex<<std::endl;
+                            int leftOperand = std::stoi(expression.substr(leftIndex+1,index-leftIndex-1));
+                            if(expression[leftIndex] == '-')
+                                leftOperand = -leftOperand;
+                            rightIndex = index+1;
+                            if(rightIndex>=size)
+                            {
+                                //1. rightIndex>=size, out of range, no right operand
+                                std::cout<<"\t no right operand"<<std::endl;
+                                result = leftOperand;
+                            }
+                            else
+                            {
+                                //2. rightIndex<size, in range, try take right operand
+                                std::cout<<"\t rightIndex<size"<<std::endl;
+                                while((rightIndex<size)&&(expression[rightIndex]>=48)&&(expression[rightIndex]<=57))
+                                {
+                                    rightIndex++;
+                                }
+                                if(rightIndex>=size)
+                                {
+                                    //1. rightIndex out of range
+                                    rightIndex--;
+                                    int rightOperand = std::stoi(expression.substr(index+1,rightIndex-index));
+                                    if(expression[index] == '+')
+                                    {
+                                        result = leftOperand + rightOperand;
+                                    }
+                                    else
+                                    {
+                                        result = leftOperand - rightOperand;
+                                    }
+                                    std::cout<<"\t"<<leftOperand<<std::string(1,expression[index])<<rightOperand<<"="<<result<<std::endl;
+                                    std::string substr = std::to_string(result);
+                                    expression.insert(index,substr);
+                                    expression.erase(index+substr.size(),rightIndex-leftIndex);
+                                    std::cout<<"\tafter compute, expression:"<<expression<<std::endl;
+                                }
+                                else
+                                {
+                                    //2. rightIndex in range
+                                    std::cout<<"\trightIndex in range, rightIndex:"<<rightIndex<<std::endl;
+                                    if(rightIndex == index+1)
+                                    {
+                                        //may be a signed number
+                                        int tempIndex = rightIndex;
+                                        while((tempIndex<size)&&((expression[tempIndex]=='+')||(expression[tempIndex]=='-')))
+                                        {
+                                            tempIndex++;
+                                        }
+                                        if(tempIndex>=size)
+                                        {
+                                            std::cout<<"\tillegal, noright number, tempIndex:"<<tempIndex<<std::endl;
+                                            legal = false;
+                                            result = 0;
+                                        }
+                                        else
+                                        {
+                                            std::cout<<"\t right operand is a signed number"<<std::endl;
+                                            int negative = 1;
+                                            for(int i=index+1;i<tempIndex;i++)
+                                            {
+                                                if(expression[i] == '-')
+                                                    negative*=-1;
+                                            }
+                                            rightIndex = tempIndex;
+                                            while((rightIndex<size)&&(expression[rightIndex]>=48)&&(expression[rightIndex]<=57))
+                                            {
+                                                rightIndex++;
+                                            }
+                                            int rightOperand = std::stoi(expression.substr(tempIndex,rightIndex-tempIndex));
+                                            rightOperand*=negative;
+                                            if(expression[index] == '+')
+                                                result = leftOperand+rightOperand;
+                                            else
+                                                result = leftOperand-rightOperand;
+                                            std::string substr = std::to_string(result);
+                                            expression.insert(leftIndex,substr);
+                                            expression.erase(leftIndex+substr.size(),rightIndex-leftIndex);
+                                            std::cout<<"\tcompute "<<leftOperand<<std::string(1,expression[index])<<rightOperand<<"="<<result<<" substr:"<<substr<<" after comput expression"<<expression<<std::endl;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        int rightOperand = std::stoi(expression.substr(index+1,rightIndex-index));
+                                        if(expression[index] == '+')
+                                            result = leftOperand+rightOperand;
+                                        else
+                                        {
+                                            result = leftOperand-rightOperand;
+                                        }
+                                        std::cout<<"\t"<<leftOperand<<std::string(1,expression[index])<<rightOperand<<"="<<result<<std::endl;
+                                        std::string substr = std::to_string(result);
+                                        expression.insert(index,substr);
+                                        expression.erase(index+substr.size(),rightIndex-leftIndex);
+                                        std::cout<<"\tafter compute, expression:"<<expression<<std::endl;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        //1. rightIndex >=size, out of range
+                        //the position is like
+                        //1. index,rightIndex
+                        //2. index, ..., rightIndex
+                        if(index == size-1)
+                        {
+                            //1. index,rightIndex
+                            std::cout<<"\tillegal, index == size-1, operator has no right number:"<<rightIndex<<std::endl;
+                            legal = false;
+                            result = 0;
+                        }
+                        else
+                        {
+                            //2. index, ..., rightIndex
+                            std::cout<<"\tindex,...,rightIndex, rightIndex:"<<rightIndex<<std::endl;
+                            if(expression[index] == '+')
+                                result = std::stoi(expression.substr(index+1));
+                            else
+                                result = std::stoi(expression);
+                            std::cout<<"\tit's a number, no expression to compute, expression:"<<expression<<std::endl;
+                            return result;
+                        }
+                    }
                 }
-                result = 12;
+                else
+                {
+                    //2. leftIndex>=0, index>=0, must be a operator
+                    std::cout<<"\tleftIndex >=0 index>=0, must be a operator, leftIndex:"<<leftIndex<<std::endl;
+                    int leftOperand = std::stoi(expression.substr(leftIndex,index-leftIndex));
+                    if(index == size-1)
+                    {
+                        std::cout<<"\tillegal, operator is the last char, index:"<<index<<std::endl;
+                        legal= false;
+                        result = 0;
+                    }
+                    else
+                    {
+                        int rightIndex = index+1;
+                        while((rightIndex<size)&&(expression[rightIndex]>=48)&&(expression[rightIndex]<=57))
+                        {
+                            rightIndex++;
+                        }
+                        if(rightIndex<size)
+                        {
+                            if(rightIndex == index+1)
+                            {
+                                int tempIndex = rightIndex;
+                                while((tempIndex<size)&&((expression[tempIndex]=='+')||(expression[tempIndex]=='-')))
+                                {
+                                    tempIndex++;
+                                }
+                                if(tempIndex>=size)
+                                {
+                                    std::cout<<"\tillegal, all operator, no number"<<std::endl;
+                                    legal = false;
+                                    result = 0;
+                                }
+                                else{
+                                    int negative = 1;
+                                    for(int i=rightIndex;i<tempIndex;i++)
+                                        if(expression[i]=='-')
+                                            negative*=-1;
+                                    rightIndex = tempIndex;
+                                    while((rightIndex<size)&&(expression[rightIndex]>=48)&&(expression[rightIndex]<=57))
+                                    {
+                                        rightIndex++;
+                                    }
+                                    if(rightIndex > size)
+                                        rightIndex--;
+                                    int rightOperand = std::stoi(expression.substr(tempIndex,rightIndex-tempIndex));
+                                    if(expression[index] == '+')
+                                        result = leftOperand+rightOperand;
+                                    else
+                                        result = leftOperand-rightOperand;
+                                    std::string substr = std::to_string(result);
+                                    expression.insert(leftIndex,substr);
+                                    expression.erase(leftIndex+substr.size(),rightIndex-leftIndex);
+                                    std::cout<<"\t"<<leftOperand<<std::string(1,expression[index])<<rightOperand<<"="<<result<<" after compute expression:"<<expression<<std::endl;
+                                    result = computeExpression(expression,legal);
+                                }
+                            }
+                            else
+                            {
+                                int rightOperand = std::stoi(expression.substr(index+1,rightIndex-index));
+                                if(expression[index] == '+')
+                                    result = leftOperand+rightOperand;
+                                else
+                                    result = leftOperand-rightOperand;
+                                std::string substr = std::to_string(result);
+                                expression.insert(leftIndex,substr);
+                                expression.erase(leftIndex+substr.size(),rightIndex-leftIndex);
+                                std::cout<<"\t "<<leftOperand<<std::string(1,expression[index])<<rightOperand<<"="<<result<<" after compute, expression:"<<expression<<std::endl;
+                            }
+                        }
+                        else
+                        {
+                            int rightOperand = std::stoi(expression.substr(index+1,rightIndex-index));
+                            if(expression[index] == '+')
+                                result = leftOperand+rightOperand;
+                            else
+                                result = leftOperand-rightOperand;
+                            std::string substr = std::to_string(result);
+                            expression.insert(leftIndex,substr);
+                            expression.erase(leftIndex+substr.size(),rightIndex-leftIndex);
+                            std::cout<<"\t "<<leftOperand<<std::string(1,expression[index])<<rightOperand<<"="<<result<<" after compute, expression:"<<expression<<std::endl;
+                        }
+                    }
+                }
             }
         }
         else
